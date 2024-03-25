c10f35842145d6cbf1778c1b4f377b88adccb820
RicHard <richardicode@gmail.com>
2024-03-25 15:04:22 +0800

    adding bigbark()

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/RicHardicode/puppy
 
 go 1.22.0
+
+require github.com/RicHardicode/dog v0.0.0-20240325065349-7ffb2ffce847
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/RicHardicode/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func Bigbark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func Bigbarks() string {
+	return dog.WhenGrownUp(Barks())
 }
